82036c6fa206a9c409647451f53248d8d6126ae8
Jonathan SM Zea <jonathan.a.zea@ieee.org>
2024-02-06 12:18:09 -0500

    works by console, in real time, without post-processing

---
+++ eval_realTime.m
@@ -0,0 +1,46 @@
+%eval_realTime runs the model Model_spec_CNN_LSTM in real time without post-processing.
+
+%{
+Laboratorio de Inteligencia y Visión Artificial
+ESCUELA POLITÉCNICA NACIONAL
+Quito - Ecuador
+
+autor: jonathan.zea
+jonathan.a.zea@ieee.org
+
+"I find that I don't understand things unless I try to program them."
+-Donald E. Knuth
+
+12 May 2023
+%}
+
+cc
+%% Configs
+window_size = 300;
+stride = 30;
+
+
+%% Aux and dependent variables
+% libs
+% addpath(genpath('..\..\libraries'))
+cd()
+period = 1/200* 30;
+
+%%
+myo = Myo();
+model = Model_spec_CNN_LSTM();
+graph = EMG_plot(window_size);
+
+%% loop
+while true
+    t = tic;
+    emg = myo.load_EMG_window(window_size);
+    class_pred = model.classify(emg);
+    drawnow
+    %     graph.update_emg(emg);
+    drawnow
+    rem = period - toc(t);
+    Print(sprintf("%.2f %s\n", rem, class_pred(end)));
+    
+    pause(period - toc(t))
+end
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Real-Time Hand Gesture Recognition
+
+Uses the model 2023-HGR5-CNN_LSTM
+
+# TODO
+* post-processing needs to be implemented.
